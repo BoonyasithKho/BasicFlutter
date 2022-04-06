618fbdd7d9ef614f03a057c2cc00ed46a8ddf6ed
BoonyasithKho <boonyasith@gistda.or.th>
2022-04-06 17:33:26 +0700

    ListViewBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+import 'dart:ui';
+import 'food_menu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: MyHomePage(),
+      home: const MyHomePage(),
       // home: Scaffold(
       //     appBar: AppBar(
       //       title: Text("Nerd"),
@@ -19,13 +22,13 @@ class MyApp extends StatelessWidget {
       //     body: Center(
       //       child:
       //           Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      //         Text(
+      //         const Text(
       //           "Hello Flutter",
       //           style: TextStyle(fontSize: 50, color: Colors.blue),
       //         ),
       //         Row(
       //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //           children: [
+      //           children: const [
       //             Image(
       //               image: NetworkImage(
       //                   'https://cdn.pixabay.com/photo/2022/03/07/18/41/forest-7054278_960_720.jpg'),
@@ -37,7 +40,11 @@ class MyApp extends StatelessWidget {
       //               width: 150,
       //             )
       //           ],
-      //         )
+      //         ),
+      //         const Image(
+      //           image: AssetImage("assets/images/img1.jpeg"),
+      //           width: 150,
+      //         ),
       //       ]),
       //     )),
       theme: ThemeData(primarySwatch: Colors.green),
@@ -52,35 +59,115 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// class _MyHomePageState extends State<MyHomePage> {
+//   int number = 0; //การสร้าง State หรือตัวแปร
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // ส่วนการเตรียมข้อมูล เก็บลงใน Data ก่อนนำเข้าไปแสดงใน children
+//     // List<Widget> data = [];
+//     // data.add(const Text("Hello Flutter",
+//     //     style: TextStyle(fontSize: 50, color: Colors.blue)));
+//     // data.add(const Text("กดปุ่มเพื่อเพิ่มจำนวน"));
+//     // data.add(Text(
+//     //   "$number",
+//     //   style: const TextStyle(fontSize: 40, color: Colors.red),
+//     // ));
+//     // for (var i = 0; i < 100; i++) {
+//     //   data.add(Text("Flutter ${i + 1}"));
+//     // }
+
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Nerd"),
+//         ),
+//         // body: Center(
+//         //   // child: Column(
+//         //   //     mainAxisAlignment: MainAxisAlignment.start,  // ใช้ได้ใน Column กับ Row เท่านั้น
+//         //   child: ListView(
+//         //       children: getData(15)),
+//         body: ListView.builder(
+//           itemBuilder: (BuildContext context, int index) {
+//             return ListTile(
+//               title: Text("เมนูที่ ${index + 1}"),
+//             );
+//           },
+//           itemCount: 10,
+//         )
+
+//         // floatingActionButton: FloatingActionButton(
+//         //   onPressed: addNumber,
+//         //   child: const Icon(Icons.add),
+//         // )
+//         );
+//   }
+
+//   // void addNumber() {
+//   //   setState(() {
+//   //     number++;
+//   //   });
+//   // }
+
+//   // List<Widget> getData(int count) {
+//   //   List<Widget> data = [];
+//   //   for (var i = 0; i < count; i++) {
+//   //     // var menu = Text(
+//   //     //   "Flutter ${i + 1}",
+//   //     //   style: const TextStyle(fontSize: 30, color: Colors.pink),
+//   //     // );
+//   //     var menu = ListTile(
+//   //       title: Text(
+//   //         "เมนูที่ ${i + 1}",
+//   //         style: const TextStyle(color: Colors.blue),
+//   //       ),
+//   //       subtitle: Text("รายการย่อยที่ ${i + 1}"),
+//   //     );
+
+//   //     data.add(menu);
+//   //   }
+//   //   return data;
+//   // }
+// }
+
+// ลองแสดงผลการเลือกเมนูอาหาร
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0; //การสร้าง State หรือตัวแปร
+  List<FoodMenu> menu = [
+    FoodMenu("กุ้งเผา", "500", "assets/images/img1.jpeg"),
+    FoodMenu("กะเพราหมู", "80", "assets/images/img2.jpeg"),
+    FoodMenu("ส้มตำ", "60", "assets/images/img3.jpeg"),
+    FoodMenu("ผัดไท", "40", "assets/images/img4.jpeg"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Nerd"),
-        ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              "Hello Flutter",
-              style: TextStyle(fontSize: 50, color: Colors.blue),
+      appBar: AppBar(title: const Text("เลือกเมนูอาหาร")),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          FoodMenu food = menu[index];
+          return ListTile(
+            tileColor: Colors.grey,
+            leading: Image.asset(
+              food.img,
+              cacheWidth: 70,
+              cacheHeight: 50,
             ),
-            Text(
-              "Hello Flutter",
-              style: TextStyle(fontSize: 50, color: Colors.blue),
+            title: Text(
+              food.name,
+              style: const TextStyle(color: Colors.white),
             ),
-            Text("กดปุ่มเพื่อเพิ่มจำนวน"),
-            Text(
-              "$number",
-              style: TextStyle(fontSize: 40, color: Colors.red),
-            )
-          ]),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ));
+            subtitle: Text(
+              "ราคา ${food.price} บาท",
+              style: const TextStyle(color: Colors.white),
+            ),
+            onTap: () {
+              print("คุณเลือกเมนูอาหารชื่อว่า " + food.name);
+            },
+          );
+        },
+        itemCount: menu.length,
+      ),
+      backgroundColor: Colors.grey,
+    );
   }
 }
